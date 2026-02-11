c721bb4ea3ffa331165126ae512377118f865e30
soilflux <52200608+soilflux@users.noreply.github.com>
2026-02-10 19:04:40 -0500

    show values for current day

--- calendar.js
+++ calendar.js
@@ -5,5 +5,15 @@ const month = String(now.getMonth() + 1).padStart(2, '0');
 const fullYear = now.getFullYear();
 year = String(fullYear).slice(-2);
 
+let id = "a";
+
 console.log("images/aWinners/" + month + day + year + ".png");
-document.body.style.setProperty('--bg-image', "url(images/aWinners/" + month + day + year + ".png)");
+document.body.style.setProperty('--bg-image', "url(images/" + id + "Winners/" + month + day + year + ".png)");
+
+
+todaysText = daysText.split(month + day + year);
+todaysText = todaysText[1].split(":::" + id);
+todaysText = todaysText[1].split("\n");
+document.getElementById("dayText").innerHTML = todaysText[1] + "<br>" + todaysText[2];
+console.log(todaysText[1]);
+
